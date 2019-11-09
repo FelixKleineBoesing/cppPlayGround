d7eaeedef71f928dc269fceab694bef2657520cc
Felix Kleine Bösing <felix.boesing@t-online.de>
2019-11-09 15:26:51 +0100

    remove everything

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,5 @@
 #include <iostream>
-#include <vector>
-#include <string>
 
-using namespace std;
-
-int main()
-{
-
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+int main() {
+    std::cout << "Hello Easy C++ project!" << std::endl;
 }
